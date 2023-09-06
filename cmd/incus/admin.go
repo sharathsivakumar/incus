aede9c22387f8ff8f4c7ff16010ef16b5d9d16c1
sharathsivakumar <mailssr9@gmail.com>
2023-09-06 07:39:44 +0200

    incus/cmd: Add admin sub command and move init subcommand from incusd
    Signed-off-by: sharathsivakumar <mailssr9@gmail.com>

---
+++ cmd/incus/admin.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	cli "github.com/lxc/incus/shared/cmd"
+	"github.com/lxc/incus/shared/i18n"
+	"github.com/spf13/cobra"
+)
+
+type cmdAdmin struct {
+	global *cmdGlobal
+}
+
+func (c *cmdAdmin) Command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("admin")
+	cmd.Short = i18n.G("Manage incus deamon")
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`Manage incus daemon`))
+
+	// init
+	adminInitCmd := cmdAdminInit{global: c.global}
+	cmd.AddCommand(adminInitCmd.Command())
+
+	return cmd
+}
